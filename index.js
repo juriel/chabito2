9f244ba3e9f1822646e05fef5584018cf7a519e1
Jaime Uriel Torres Serrano <juriel@comtor.net>
2026-03-27 21:08:58 -0500

    style: enable small QR code rendering in terminal output

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ sock.ev.on('connection.update', async (update) => {
     // In prod, send this string to your frontend then generate the QR there
     if (qr) {
         // as an example, this prints the qr code to the terminal
-        console.log(await QRCode.toString(qr, { type: 'terminal' }))
+        console.log(await QRCode.toString(qr, { type: 'terminal', small: true }))
     }
 })
 
